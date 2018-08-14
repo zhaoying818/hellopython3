9cefc65c851a7433d604f786e097296fdb0457bd
zhaoying <zhaoying1258@qq.com>
2018-08-14 23:39:19 +0800

    pytest --pep8 xx.py

--- ch04/mymodules/vsearch.py
+++ ch04/mymodules/vsearch.py
@@ -1,9 +1,9 @@
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
     """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
-def search4letters(phrase:str, letters:str='aoeiu') -> set:
+def search4letters(phrase: str, letters: str='aoeiu') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
