7646f1521f91aba236ea8a60dd99a6866b5a7438
zhaoying <zhaoying1258@qq.com>
2018-08-14 23:15:07 +0800

    package a module

---
+++ ch04/mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools.',
+    author='Zhao Ying',
+    author_email='zhaoying1258@qq.com',
+    url='clouddevops.xin',
+    py_modules=['vsearch']
+    )
